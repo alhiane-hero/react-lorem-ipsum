5b4d687b9798f7c27a199ec7d13ef252d92a1840
alhiane-hero <alhiane.hero1998@gmail.com>
2021-11-07 12:38:05 +0100

    React Lorem Ipsum App Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import data from './Data';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (amount > 8) {
+      amount = 8;
+    }
+    if (amount <= 0) {
+      amount = 1;
+    }
+    setText(data.slice(0, amount));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Container'>
+        <h3 className='Title'>
+          TIRED OF BORING LOREM IPSUM?
+        </h3>
+        <form action='#' onSubmit={handleSubmit}>
+          <label for='amount'>Paragraphs:</label>
+          <input type='number' id='amount' value={count}
+          onChange={e => setCount(e.target.value)}/>
+          <input type='submit' value='Generate' />
+        </form>
+        <div className='Textes'>{
+          text.map(txt => {
+            return <p>{txt}</p>
+          })
+        }</div>
+      </div>
     </div>
   );
 }
